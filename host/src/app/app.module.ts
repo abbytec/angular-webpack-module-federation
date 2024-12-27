f6d36e6a67e2a42134e25bc33da5b28a4a9a4493
Abbytec <gpsmurfs@gmail.com>
2024-12-27 18:21:51 -0300

    fix: host

--- host/src/app/app.module.ts
+++ host/src/app/app.module.ts
@@ -4,16 +4,24 @@ import { RouterModule, Routes } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { AppComponent } from "./app.component";
+import "zone.js";
 
 const routes: Routes = [
-	{
-		path: "about-us",
-		loadChildren: () => import("aboutUs/AboutUsModule").then((m) => m.AboutUsModule),
-	},
-	{
-		path: "homepage",
-		loadChildren: () => import("homePage/HomePageModule").then((m) => m.HomePageModule),
-	},
+  {
+    path: "",
+    redirectTo: "homepage", // Redirige a la página deseada
+    pathMatch: "full", // Asegúrate de usar "full" para coincidir con la raíz exacta
+  },
+  {
+    path: "about-us",
+    loadChildren: () =>
+      import("aboutUs/AboutUsModule").then((m) => m.AboutUsModule),
+  },
+  {
+    path: "homepage",
+    loadChildren: () =>
+      import("homePage/HomePageModule").then((m) => m.HomePageModule),
+  },
 ];
 
 @NgModule({
--- host/webpack.config.js
+++ host/webpack.config.js
@@ -2,63 +2,97 @@ const path = require('path');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'development',
-  entry: './src/bootstrap.ts', // Archivo de entrada para Angular
+  mode: "development",
+  entry: "./src/bootstrap.ts", // Archivo de entrada para Angular
   devServer: {
     port: 4200,
     static: {
-      directory: path.join(__dirname, 'src', 'public'),
+      directory: path.join(__dirname, "src", "app"),
     },
     historyApiFallback: true,
   },
   output: {
-    uniqueName: 'host',
-    publicPath: 'http://localhost:4200/',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    uniqueName: "host",
+    publicPath: "http://localhost:4200/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
     clean: true,
   },
   resolve: {
-    extensions: ['.ts', '.js'], // Asegúrate de incluir extensiones TypeScript
+    extensions: [".ts", ".js", ".html", ".css"], // Asegúrate de incluir extensiones TypeScript
+    modules: [__dirname, "node_modules", "src/app"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        use: 'raw-loader', // Procesa archivos HTML como cadenas de texto
+        include: path.resolve(__dirname, "src", "app"),
+        use: "html-loader", // Procesa archivos HTML como cadenas de texto
       },
       {
         test: /\.css$/,
-        use: ['to-string-loader', 'css-loader'], // Convierte CSS en cadenas de texto
+        include: path.resolve(__dirname, "src", "app"),
+        use: ["style-loader", "css-loader"], // Convierte CSS en cadenas de texto
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/, // Opcional: Soporte para imágenes
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
       remotes: {
-        aboutUs: 'aboutUs@http://localhost:4201/remoteEntry.js',
-        homePage: 'homePage@http://localhost:4202/remoteEntry.js',
+        aboutUs: "aboutUs@http://localhost:4201/remoteEntry.js",
+        homePage: "homePage@http://localhost:4202/remoteEntry.js",
       },
       shared: {
-        '@angular/core': { singleton: true, strictVersion: false, requiredVersion: false, eager: true },
-        '@angular/common': { singleton: true, strictVersion: false, requiredVersion: false, eager: true },
-        '@angular/router': { singleton: true, strictVersion: false, requiredVersion: false, eager: true },
-        '@angular/platform-browser': { singleton: true, strictVersion: false, requiredVersion: false, eager: true },
-        '@angular/animations': { singleton: true, strictVersion: false, requiredVersion: false, eager: true },
+        "@angular/core": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
+        "@angular/platform-browser": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
+        "@angular/animations": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
+        "@angular/platform-browser-dynamic": {
+          singleton: true,
+          strictVersion: false,
+          requiredVersion: false,
+          eager: true,
+        },
       },
     }),
     new HtmlWebpackPlugin({
-      template: './src/public/index.html', // Ruta a tu index.html
-      inject: 'body', // Inyecta los scripts en el cuerpo
+      template: "./src/public/index.html", // Ruta a tu index.html
+      inject: "body", // Inyecta los scripts en el cuerpo
     }),
-  ],
+  ]
 };
